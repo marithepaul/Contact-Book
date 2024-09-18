16deeacbc9e60d42c86c5175ce79553152b825ce
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-09-18 12:26:34 -0500

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,2 +1,12 @@
 DROP DATABASE IF EXISTS Contact_Book;
 CREATE DATABASE Contact_Book;
+
+USE Contact_Book;
+
+CREATE TABLE Contacts (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20),
+  email VARCHAR(20),
+  phone_number VARCHAR(12),
+  address VARCHAR(25)
+  );
