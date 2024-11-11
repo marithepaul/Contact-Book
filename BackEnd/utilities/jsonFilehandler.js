e57590d8254e3670afeb0b251e473a2492fdb3c4
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-11-11 11:47:07 -0600

    Update jsonFilehandler.js

--- BackEnd/utilities/jsonFilehandler.js
+++ BackEnd/utilities/jsonFilehandler.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const readJSON = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path.join(__dirname, '..', filePath), 'utf8', (err, data) => {
+            if (err) reject(err);
+            else resolve(JSON.parse(data));
+        });
+    });
+};
+
+const writeJSON = (filePath, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path.join(__dirname, '..', filePath), JSON.stringify(data, null, 2), 'utf8', (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+};
+
+module.exports = { readJSON, writeJSON };
 
