0b46bc661b135f4ed2d5c9148480cd7e67f1461b
Conor2025 <146126867+Conor2025@users.noreply.github.com>
2024-11-11 11:42:33 -0600

    Update authentication.js

--- BackEnd/middleware/authentication.js
+++ BackEnd/middleware/authentication.js
@@ -1,1 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const auth = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.status(401).send('Access denied.');
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token.');
+    }
+};
+
+module.exports = auth;
 
